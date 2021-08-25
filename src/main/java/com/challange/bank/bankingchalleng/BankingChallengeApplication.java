e6bc94525d195bcb99e1369d4b6862325f0e535d
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2021-08-25 20:23:15 +0200

    (server-config): configure app

---
+++ src/main/java/com/challange/bank/bankingchalleng/BankingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challange.bank.bankingchalleng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingChallengeApplication.class, args);
+    }
+
+}
